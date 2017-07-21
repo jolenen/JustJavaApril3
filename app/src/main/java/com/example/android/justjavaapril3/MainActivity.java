9b6cbc06ccfcd4bef29b4db6facf5d4c040b2eca
jolenen <jolenen@users.noreply.github.com>
2017-07-21 10:07:54 -0700

    Update submitOrder to say Thank You on new line

--- app/src/main/java/com/example/android/justjavaapril3/MainActivity.java
+++ app/src/main/java/com/example/android/justjavaapril3/MainActivity.java
@@ -40,20 +40,20 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view){
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view){
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      *This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
